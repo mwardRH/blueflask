714ae71dc0d4d42832ca14eeea44588fc63064a9
Matthew Ward <mward@redhat.com>
2017-06-01 16:17:44 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,15 @@
+import socket
+
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello BLUE!"
+
+@app.route("/hostname/")
+def return_hostname():
+    return "This is an example wsgi app served from {} to {}".format(socket.gethostname(), request.remote_addr)
 
 if __name__ == "__main__":
     application.run()
