cf26f1faedf8d15d666e04cabafd4e836ed73f97
root <root@earth.lab>
2017-09-07 09:10:37 -0400

    Made flask project more pythonic. It now renders an HTML page and doesn't just return an output.

---
+++ apiCall.py
@@ -0,0 +1,15 @@
+import requests
+import sys
+import time
+
+def main():
+    uri = sys.argv[1]
+    i = 0
+    while  i != 10:
+        r = requests.get(uri)
+        print(r.content)
+        time.sleep(1)
+        i = i+1
+
+if __name__ == '__main__':
+    main()
